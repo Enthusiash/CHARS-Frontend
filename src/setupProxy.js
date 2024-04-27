db0e5d7b42bd0a14ebf8728aaf353942a1de94ff
Syntaxsh <ashleyotchengco@gmail.com>
2024-04-27 16:06:18 +0800

    add render proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
 	app.use(
 		["/main", "/validuser", "/validadmin", "/validsupplier", "/uploads"],
 		createProxyMiddleware({
-			target: "http://localhost:5000" || "https://chars-store.herokuapp.com:5000" || "chars-backend.onrender.com:5000",
+			target: "http://localhost:5000" || "https://chars-store.herokuapp.com:5000" || "https://chars-backend.onrender.com:5000",
 		})
 	);
 };
